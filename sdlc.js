979838205157fa9b866ce788f8ac1b42d4f983c3
Britaney Fleming <britaneyrfleming@gmail.com>
2022-11-04 14:32:54 -0500

    second commit

--- sdlc.js
+++ sdlc.js
@@ -12,6 +12,13 @@
 // Testing & Integration
 // Planning
 
+//1. Planning
+//2. Analysis of Requirement
+//3. Design
+//4. Implementation
+//5. Testing & Integration
+//6. Maintain
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +26,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirement = "This is the second step is to define and document the functional requirements for the web app to work and meet the client or customers needs."
+const design = "The third step involves designing a basic model of the software and it will work."
+const implementation = "This is the step where the code for the software is written."
+const testingAndIntegration = "This step is where the code is tested for bugs and errors to determine if it meets the goals set in steps one and two."
+const maintenance = "The sixth and final step in the process, involves adding in new features and fixing bugs and errors as needed."
